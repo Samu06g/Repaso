d2fc7698fbf4f9053e68a71b62f9a6e5ab2d2c73
Samu06g <samuel.gonzaleztrueba@iesmiguelherrero.com>
2022-12-23 19:18:02 +0100

    repositorio con modificacion

--- repaso/src/main/java/repaso/repaso/Repaso.java
+++ repaso/src/main/java/repaso/repaso/Repaso.java
@@ -14,5 +14,6 @@ public class Repaso {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("hola");
+        System.out.println("jajajajajajajajajajajaja");
     }
 }
