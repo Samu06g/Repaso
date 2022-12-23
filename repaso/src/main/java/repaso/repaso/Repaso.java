4bdc5dab774e6405c6202eab76ea9b49d1974453
Samu06g <119075868+Samu06g@users.noreply.github.com>
2022-12-23 19:14:23 +0100

    Conflicto

--- repaso/src/main/java/repaso/repaso/Repaso.java
+++ repaso/src/main/java/repaso/repaso/Repaso.java
@@ -12,7 +12,7 @@ package repaso.repaso;
 public class Repaso {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("jlkñjkljklñ!");
         System.out.println("hola");
     }
 }
